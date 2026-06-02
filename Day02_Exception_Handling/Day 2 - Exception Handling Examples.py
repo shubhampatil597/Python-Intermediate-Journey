7d430f68ffed99f97188ff764dfe060bdada1ade
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-02 19:49:16 +0530

    Create Day 2 - Exception Handling Examples.py

---
+++ Day02_Exception_Handling/Day 2 - Exception Handling Examples.py
@@ -0,0 +1,27 @@
+Day 2 - Exception Handling Examples
+
+print("Example 1 : ValueError")
+
+try:
+number = int(input("Enter a Number: "))
+print("You entered:", number)
+
+except ValueError:
+print("Invalid Input! Numbers only.")
+
+print("\n---------------------")
+
+print("Example 2 : ZeroDivisionError")
+
+try:
+num = int(input("Enter Number: "))
+result = 100 / num
+
+except ZeroDivisionError:
+print("Cannot divide by zero")
+
+else:
+print("Result =", result)
+
+finally:
+print("Program Finished")
