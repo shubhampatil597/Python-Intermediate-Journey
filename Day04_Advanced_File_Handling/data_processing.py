a3c85958c3d5ad80048a0f3d3c1e339c0c7c3f22
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:18:27 +0530

    Update data_processing.py

--- Day04_Advanced_File_Handling/data_processing.py
+++ Day04_Advanced_File_Handling/data_processing.py
@@ -1,1 +1,16 @@
-a
+import csv
+
+file = open("marks.csv", "r")
+
+reader = csv.reader(file)
+
+next(reader)
+
+total = 0
+
+for row in reader:
+    total += int(row[1])
+
+print("Total Marks =", total)
+
+file.close()
