23ae392d0dcb895609ff5076daabc4643c657556
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:19:04 +0530

    Update json_read.py

--- Day04_Advanced_File_Handling/json_read.py
+++ Day04_Advanced_File_Handling/json_read.py
@@ -1,1 +1,11 @@
-a
+import json
+
+file = open("student.json", "r")
+
+data = json.load(file)
+
+print("Name:", data["name"])
+print("Age:", data["age"])
+print("City:", data["city"])
+
+file.close()
