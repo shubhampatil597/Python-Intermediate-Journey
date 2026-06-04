f164dec8dd0862e5f9ea8b81994bd87558d65c21
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:19:20 +0530

    Update json_write.py

--- Day04_Advanced_File_Handling/json_write.py
+++ Day04_Advanced_File_Handling/json_write.py
@@ -1,1 +1,15 @@
-a
+import json
+
+student = {
+    "name": "Shubham",
+    "age": 20,
+    "city": "Kolhapur"
+}
+
+file = open("student.json", "w")
+
+json.dump(student, file)
+
+file.close()
+
+print("JSON File Created Successfully")
