c977bcd800220cc901b7766dde648c9d4793ddab
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:20:43 +0530

    Update student_record_processor.py

--- Day04_Advanced_File_Handling/student_record_processor.py
+++ Day04_Advanced_File_Handling/student_record_processor.py
@@ -1,1 +1,19 @@
-a
+import csv
+
+file = open("students.csv", "r")
+
+reader = csv.reader(file)
+
+next(reader)
+
+count = 0
+
+print("Student Records")
+
+for row in reader:
+    print(row)
+    count += 1
+
+print("Total Students =", count)
+
+file.close()
