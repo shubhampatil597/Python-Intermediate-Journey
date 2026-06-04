ef5351e5e26f4222c650f667d6291092cc0958a3
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:17:29 +0530

    Update average_marks.py

--- Day04_Advanced_File_Handling/average_marks.py
+++ Day04_Advanced_File_Handling/average_marks.py
@@ -1,1 +1,20 @@
-a
+import csv
+
+file = open("marks.csv", "r")
+
+reader = csv.reader(file)
+
+next(reader)
+
+total = 0
+count = 0
+
+for row in reader:
+    total += int(row[1])
+    count += 1
+
+average = total / count
+
+print("Average Marks =", average)
+
+file.close()
