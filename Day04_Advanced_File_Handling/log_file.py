92fa9aa06058edcc217dc4fb3ce9afa6d8b0d853
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-05 01:19:44 +0530

    Update log_file.py

--- Day04_Advanced_File_Handling/log_file.py
+++ Day04_Advanced_File_Handling/log_file.py
@@ -1,1 +1,9 @@
-a
+file = open("server_log.txt", "a")
+
+file.write("04-06-2026 Login Successful\n")
+file.write("04-06-2026 File Uploaded\n")
+file.write("04-06-2026 Logout\n")
+
+file.close()
+
+print("Log Updated Successfully")
