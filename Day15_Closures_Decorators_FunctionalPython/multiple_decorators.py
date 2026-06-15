eb5488e9f967aaef53e64609693bff292da1570e
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-16 01:11:16 +0530

    Create multiple_decorators.py

---
+++ Day15_Closures_Decorators_FunctionalPython/multiple_decorators.py
@@ -0,0 +1,30 @@
+def decorator1(func):
+
+    def wrapper():
+
+        print("Decorator 1")
+
+        func()
+
+    return wrapper
+
+
+def decorator2(func):
+
+    def wrapper():
+
+        print("Decorator 2")
+
+        func()
+
+    return wrapper
+
+
+@decorator1
+@decorator2
+def greet():
+
+    print("Hello")
+
+
+greet()
