5d5219ef29953f533d96a57bf24db29dada28726
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-19 21:09:49 +0530

    Create centralized_logging_demo.py

---
+++ Day19_AdvancedLogging/centralized_logging_demo.py
@@ -0,0 +1,23 @@
+servers = [
+
+    "Server-1",
+
+    "Server-2",
+
+    "Server-3"
+
+]
+
+for server in servers:
+
+    print(
+
+        f"Collecting Logs From {server}"
+
+    )
+
+print(
+
+    "Sending Logs To Central Dashboard"
+
+)
