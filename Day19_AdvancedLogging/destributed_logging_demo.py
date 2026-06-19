0712e6c7519d7df9e879e9100d1f4a917d1ac541
shubhampatil597 <shubhampatil8554081995@gmail.com>
2026-06-19 21:10:23 +0530

    Create destributed_logging_demo.py

---
+++ Day19_AdvancedLogging/destributed_logging_demo.py
@@ -0,0 +1,19 @@
+services = [
+
+    "Auth Service",
+
+    "Payment Service",
+
+    "Notification Service"
+
+]
+
+request_id = "REQ777"
+
+for service in services:
+
+    print(
+
+        f"{request_id} -> {service}"
+
+    )
